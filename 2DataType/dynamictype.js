d133e65170201487694e05d55312bb5bd75c7516
Sohan Ac <sohanachhami655@gmail.com>
2026-01-23 13:31:33 +0545

    feat(truthy falsy):implement truthy and falsy value

--- 2DataType/dynamictype.js
+++ 2DataType/dynamictype.js
@@ -23,3 +23,17 @@ console.log('4'-'4')
 console.log(typeof('4'-'4'))
 
 //truthy and falsy value 
+//0 false "" null undefined NaN document.all =>it is converted to false 
+// and all others are converted to false.
+if(!NaN){
+    console.log('I am learning the javascript')
+}
+if(-1){ //this is truthy value 
+ console.log('This is truthy value')
+} 
+
+if(2234343){
+    console.log('This is also truthy value')
+}
+
+ 
