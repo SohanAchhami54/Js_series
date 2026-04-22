5e898df5c26c19b7982981d4fad6f1e8da15b518
Sohan Ac <sohanachhami655@gmail.com>
2026-04-22 16:51:51 +0545

    feat(DOM): learn dom selectors and manipulation

---
+++ 9DOM/script.js
@@ -0,0 +1,12 @@
+let h1tag=document.querySelector('h1') 
+h1tag.hidden=true
+console.log('value is:',h1tag)
+
+let para=document.querySelector('p') 
+para.textContent='I am building the simple website for myself to start the company.'
+
+console.log('Value is:',para)
+
+
+let a=document.querySelector('a') 
+a.setAttribute('href','https://www.google.com')
