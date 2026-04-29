23a48142da48bd48da32b0ca8ad5766acfe20b76
Sohan Ac <sohanachhami655@gmail.com>
2026-04-29 15:36:50 +0545

    feat(event): add form submit event to create dynamic profile card

--- 10Events/commonevent.js
+++ 10Events/commonevent.js
@@ -15,7 +15,7 @@ sel.addEventListener('change',(e)=>{
 
 const h4=document.querySelector('#h4id')  //keydown 
 window.addEventListener('keydown',(e)=>{
-    console.log(e)
+    console.log('keydown:',e)
     h4.textContent=`${e.key}`
 })
 
@@ -28,9 +28,51 @@ uploadfile.addEventListener('click',(e)=>{
     console.log(e)
 })
 hiddenfile.addEventListener('change',(e)=>{
-    const filepath=e.target.value  
-    const result=filepath.split('\\').pop()
-    console.log(result)
-    uploadfile.textContent=result 
-    
+    console.log(e)
+    // const filepath=e.target.value  //this is one way 
+    // const result=filepath.split('\\').pop()
+    // console.log(result)
+    // uploadfile.textContent=result   
+    uploadfile.textContent=e.target?.files[0]?.name //this is another way 
 })
+
+
+
+
+
+const form =document.querySelector('#cardform') 
+const main=document.querySelector('#main')
+
+const inp=document.querySelectorAll('#cardform input')
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+console.log('input value is:',inp)
+
+const card=document.createElement('div') 
+card.classList.add('card')
+
+const profile=document.createElement('div')
+profile.classList.add('profile') 
+
+const cardimg=document.createElement('img') 
+cardimg.setAttribute('id','img')
+cardimg.setAttribute('src',`${inp[2]?.value}`)
+
+const h2=document.createElement('h2') 
+h2.textContent=`${inp[0]?.value}` 
+
+const h3=document.createElement('h3') 
+h3.textContent='This is your image' 
+
+
+profile.appendChild(cardimg) 
+card.appendChild(profile) 
+card.appendChild(h2) 
+card.appendChild(h3) 
+
+main.appendChild(card)
+
+}) 
+
+
+
